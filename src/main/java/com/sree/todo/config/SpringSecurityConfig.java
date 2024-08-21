d4a096307a9b902b7f652660c840a2c7753da788
sreelu27 <sreelekshmig27@gmail.com>
2024-08-21 20:20:01 +0100

    Added spring security and in memory users

---
+++ src/main/java/com/sree/todo/config/SpringSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.sree.todo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    //basic authentication to pass username & password in header of the request
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((authorize) ->
+                        authorize.anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+    //create multiple users in memory authentication
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1= User.builder()
+                .username("priya")
+                .password(passwordEncoder().encode("priya"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin= User.builder()
+                .username("pank")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return  new InMemoryUserDetailsManager(user1, admin);
+    }
+
+    //This method uses Bcrypt algorithm to hash the password
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
